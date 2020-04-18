e7e8a76b99331fb0eeabc8436c95df3b60bddf04
Kamil Grabek <kamil.grabek@allegro.pl>
2020-04-18 15:40:36 +0200

    promise example

--- examples/promiseExamples.js
+++ examples/promiseExamples.js
@@ -1,5 +1,6 @@
 const aPromise = new Promise(((resolve) => resolve(true)));
 const bPromise = new Promise(((resolve, reject) => reject('error')));
+const cPromise = new Promise(((resolve, reject) => Math.random() >= 0.5 ? resolve(true) : reject('error')));
 
 aPromise.then(value => {
   console.log(['aPromise resoled value'], value);
@@ -7,6 +8,14 @@ aPromise.then(value => {
 
 bPromise.catch(reason => {
   console.log(['bPromise rejected reason'], reason);
+});
+
+cPromise.then(value => {
+  console.log(['cPromise resolved value'], value);
+}, error => {
+  console.log(['cPromise rejected error'], error);
+}).finally(() => {
+  console.log(['cPromise finally']);
 });
 
 // we need IFEE because JavaScript cannot handle top level async/await syntax
